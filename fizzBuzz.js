51c0f58352872083d6bc91524cf3a30e4919ae83
Nick <80440799+nickdloucks@users.noreply.github.com>
2021-10-23 10:29:06 -0700

    removed "switch case" stucture

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -5,17 +5,23 @@
   * if number is divisible by both 3 and 5, print "fizzbuzz" instead of the number.
   */
 
-let count = 1; // initialize counter
-for (let i=0; i<100; i++){ // loop through the number range
-  switch(count){ // 4 conditions:
-    case ((count % 3 === 0) && (count % 5 === 0)): // number is divisible by both 3 and 5, print "fizzbuzz"
-      console.log("FizzBuzz");
-    case ((count % 3 === 0) && (!count % 5 === 0)): // number is divisible by 3, print "fizz"
-      console.log("Fizz");
-    case ((count % 5 === 0) && (!count % 3 === 0)): // number is divisible by 5, print "buzz"
-      console.log("Buzz");
-    default: // reaching the default case means the number is divisible by neither 3 nor 5, so just print the number.
-      console.log(count);
-  }
-  count++;
-}
+ let count = 1; // initialize counter
+ for (let i=0; i<100; i++){ // loop through the number range
+    if ((count % 3 === 0) && (count % 5 === 0)){
+        console.log("FizzBuzz");
+    } // number is divisible by both 3 and 5, print "fizzbuzz"
+      
+    else if ((count % 3 === 0) && (!count % 5 === 0)){
+        console.log("Fizz");
+    } // number is divisible by 3, print "fizz"
+      
+    else if ((count % 5 === 0) && (!count % 3 === 0)){
+        console.log("Buzz");
+    } // number is divisible by 5, print "buzz"
+      
+    else{
+        console.log(count);
+    } // reaching the default case means the number is divisible by neither 3 nor 5, so just print the number.
+      
+    count++;
+ }
