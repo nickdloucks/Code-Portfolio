59b7833809c2ae25b092b494b3e0d52753600a18
Nick <nickdloucks@outlook.com>
2021-12-26 18:39:16 -0800

    add type-declarations

--- karatsuba-multiply.ts
+++ karatsuba-multiply.ts
@@ -2,11 +2,11 @@ function karatsuba(x: number, y: number) : number {
   if ((x < 10) || (y < 10)) {
     return x * y;
   } else {
-    let x_string = x.toString();
-    let y_string = x.toString();
+    let x_string: string = x.toString();
+    let y_string: string = x.toString();
 
-    let n = (x_string.length > y_string.length) ? x_string.length : y_string.length;
-    let half_n = n/2; 
+    let n: number = (x_string.length > y_string.length) ? x_string.length : y_string.length;
+    let half_n: number = n/2; 
 
     let x1: number = Number(x_string.slice(0, half_n));
     let x2: number = Number(x_string.slice(half_n));
@@ -14,11 +14,11 @@ function karatsuba(x: number, y: number) : number {
     let y1: number = Number(y_string.slice(0, half_n));
     let y2: number = Number(y_string.slice(half_n));
 
-    let a = karatsuba(x1, y1);
-    let c = karatsuba(x2, y2);
-    let b = karatsuba((x1 + x2), (y1 + y2)) - a - c;
+    let a: number = karatsuba(x1, y1);
+    let c: number = karatsuba(x2, y2);
+    let b: number = karatsuba((x1 + x2), (y1 + y2)) - a - c;
 
-    let product = a * 10**(2*half_n) + (b * 10**half_n) + c;
+    let product: number = a * 10**(2*half_n) + (b * 10**half_n) + c;
     return product;
   }
 }
