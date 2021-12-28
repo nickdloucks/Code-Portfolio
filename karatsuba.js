b962962bd3422cffa3d46856e873fd261e3f1e90
Nick <nickdloucks@outlook.com>
2021-12-28 15:10:29 -0800

    use math.min and change powers to terms of N

--- karatsuba.js
+++ karatsuba.js
@@ -5,7 +5,7 @@ function karatsuba(x, y)  {
       let x_string = x.toString();
       let y_string = y.toString();
   
-      let n = (x_string.length > y_string.length) ? y_string.length : x_string.length;
+      let n = Math.min(y_string.length, x_string.length);
       let half_n = Math.floor(n / 2); 
   
       let x_left = Number(x_string.slice(0, half_n));
@@ -19,11 +19,11 @@ function karatsuba(x, y)  {
       let z2 = karatsuba(x_left, y_left);
 
       // BUG MUST BE IN PRODUCT LINE
-      let product = (z2 * (10**(2*half_n))) + ((z1-z2-z0) * (10**half_n)) + z0; // xy=a∗B(2m)+b∗Bm+z0
+      let product = (z2 * (10**(2*(n/2)))) + ((z1-z2-z0) * (10**(n/2))) + z0; // xy=a∗B(2m)+b∗Bm+z0
       return product;
     }
   }
-  let result = karatsuba(123, 567);
+  let result = karatsuba(1234, 5678);
   console.log(result);
 
   /*3141592653589793238462643383279502884197169399375105820974944592, 
