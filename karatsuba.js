0e0fa5ba09d0d2890975390ac6a203e4c86999be
Nick <nickdloucks@outlook.com>
2021-12-28 21:47:54 -0800

    messing with BigInt to try and fix accuracy

--- karatsuba.js
+++ karatsuba.js
@@ -1,4 +1,8 @@
-function karatsuba(x, y)  {
+function karatsuba(num1, num2)  {
+
+    let x = BigInt(num1);
+    let y = BigInt(num2);
+
     let x_string = x.toString();
     let y_string = y.toString();
 
@@ -8,18 +12,18 @@ function karatsuba(x, y)  {
       let n = Math.max(y_string.length, x_string.length); // n = number of digits in the input numbers
       let m = Math.ceil(n / 2); 
 
-      let x_left = Math.floor(x / (10**m));
-      let x_right = x % (10**m);
+      let x_left = Math.floor(x / BigInt(10**m));
+      let x_right = x % BigInt(10**m);
 
-      let y_left = Math.floor(y / (10**m));
-      let y_right = y % (10**m);
+      let y_left = Math.floor(y / BigInt(10**m));
+      let y_right = y % BigInt(10**m);
   
       let c = karatsuba(x_right, y_right);
       let b = karatsuba((x_left + x_right), (y_left + y_right)); // b=(x_left+x_right)(y_left+y_right)−a−c
       let a = karatsuba(x_left, y_left);
 
       // BUG MUST BE IN PRODUCT LINE
-      let product = (a * (10**(2*m))) + ((b-a-c) * (10**(m))) + c; // xy=a∗B(2m)+b∗Bm+c
+      let product = (a * (10**(n))) + ((b-a-c) * (10**m)) + c; // xy=a∗B(2m)+b∗Bm+c
       return product;
     }
   }
