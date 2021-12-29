b47a33780f2f7d86bf5776be2f8117891f38cd75
Nick <nickdloucks@outlook.com>
2021-12-28 22:04:46 -0800

    more failed BigInt mixing

--- karatsuba.js
+++ karatsuba.js
@@ -1,7 +1,7 @@
 function karatsuba(num1, num2)  {
 
-    let x = BigInt(num1);
-    let y = BigInt(num2);
+    x = Number(num1);
+    y = Number(num2);
 
     let x_string = x.toString();
     let y_string = y.toString();
@@ -12,24 +12,24 @@ function karatsuba(num1, num2)  {
       let n = Math.max(y_string.length, x_string.length); // n = number of digits in the input numbers
       let m = Math.ceil(n / 2); 
 
-      let x_left = Math.floor(x / BigInt(10**m));
-      let x_right = x % BigInt(10**m);
+      let x_left = BigInt(Math.floor(x / (10**m)));
+      let x_right = BigInt(x % (10**m));
 
-      let y_left = Math.floor(y / BigInt(10**m));
-      let y_right = y % BigInt(10**m);
+      let y_left = BigInt(Math.floor(y / (10**m)));
+      let y_right = BigInt(y % (10**m));
   
-      let c = karatsuba(x_right, y_right);
-      let b = karatsuba((x_left + x_right), (y_left + y_right)); // b=(x_left+x_right)(y_left+y_right)−a−c
-      let a = karatsuba(x_left, y_left);
+      let c = BigInt(karatsuba(x_right, y_right));
+      let b = BigInt(karatsuba((x_left + x_right), (y_left + y_right))); // b=(x_left+x_right)(y_left+y_right)−a−c
+      let a = BigInt(karatsuba(x_left, y_left));
 
       // BUG MUST BE IN PRODUCT LINE
       let product = (a * (10**(n))) + ((b-a-c) * (10**m)) + c; // xy=a∗B(2m)+b∗Bm+c
-      return product;
+      return BigInt(product);
     }
   }
-  let result = karatsuba(3141592653589793238462643383279502884197169399375105820974944592,
-    2718281828459045235360287471352662497757247093699959574966967627);
-  console.log(BigInt(result).toString());
+  let result = BigInt(karatsuba(3141592653589793238462643383279502884197169399375105820974944592,
+    2718281828459045235360287471352662497757247093699959574966967627));
+  console.log(result.toString());
 
   /*3141592653589793238462643383279502884197169399375105820974944592, 
     2718281828459045235360287471352662497757247093699959574966967627 */
