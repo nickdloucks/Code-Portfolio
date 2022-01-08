172716265a28ec5ac522a5eeb5a5152af7e418b1
Nick <nickdloucks@outlook.com>
2022-01-08 11:05:36 -0800

    destructuring for multiple imports

--- Udacity exercises/modules&AsyncPractice/index.js
+++ Udacity exercises/modules&AsyncPractice/index.js
@@ -9,21 +9,21 @@
  */
 
 
-const arrayMutate = require('./utilities/arrayMutate.js');
-const arrayAnalyze = require('./utilities/arrayAnalyze.js');
+const {concat, cut3} = require('./utilities/arrayMutate.js');
+const {sum, lgNum} = require('./utilities/arrayAnalyze.js');
 
 let arrA = [1, 2, 3, 4, 5, 6, 7];
 let arrB = [8, 9, 10];
 
-let arrC = arrayMutate.concat(arrA, arrB);
+let arrC = concat(arrA, arrB);
 process.stdout.write(arrC, '\n');
 
 async function analyzeNo3 (arr, func){
-    await arrayMutate.cut3(arr);
+    await cut3(arr);
     return func(arr);
 }
-analyzeNo3(arrA, arrayAnalyze.sum).then(console.log);
+analyzeNo3(arrA, sum).then(console.log);
 
 process.on('beforeExit', () => {
-    console.log(arrayAnalyze.lgNum(arrB));
+    console.log(lgNum(arrB));
 });
