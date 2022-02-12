3dd401eb92b2ea9e55bb83ffe0e479bedc2fecf2
Nick <nickdloucks@outlook.com>
2022-02-12 08:27:36 -0800

    refactor: reaname function more intuitively

--- src/binary-to-string.ts
+++ src/binary-to-string.ts
@@ -4,7 +4,7 @@
  * @returns: A string translated to human-readable characters
  */
 
-function binaryAgent(binStr: string): string {
+function binaryTranslate(binStr: string): string {
   let binArr = binStr.split(' '); //split the string into
   //an array of bytes, one byte per character
   const biVals = [128, 64, 32, 16, 8, 4, 2, 1]; //these
@@ -30,6 +30,6 @@ function binaryAgent(binStr: string): string {
   // the resulting string
 }
 
-binaryAgent(
+binaryTranslate(
   '01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111'
 );
