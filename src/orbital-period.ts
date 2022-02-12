9979e00b90d3af916c0763b8ba5f917c0836816b
Nick <nickdloucks@outlook.com>
2022-02-11 19:46:01 -0800

    refactor: +2 new interfaces, add types, format doc

--- src/orbital-period.ts
+++ src/orbital-period.ts
@@ -1,8 +1,25 @@
-function orbitalPeriod(arr) {
-  var GM = 398600.4418;
-  var earthRadius = 6367.4447; // measure from center of Earth's core to sea level
+/**
+ * Returns the orbital period of an object in orbit.
+ * @param arr: Array<object> object format: {name, average-altitude}
+ * @returns Array<object>: object format: {name, orbital-period}
+ */
 
-  function OP(obj) {
+interface orbitingAlt { // this type is an object that gives us its average altitude
+  readonly name: string,
+  avgAlt: number,
+}
+
+export default function orbitalPeriod(arr: Array<orbitingAlt>) {
+
+  interface orbitalPeriodObj {
+    readonly name: string,
+    orbitalPeriod: number
+  }
+
+  const GM = 398600.4418;
+  const earthRadius = 6367.4447; // measure from center of Earth's core to sea level
+
+  function OP(obj: orbitingAlt): orbitalPeriodObj {
     console.log(obj.name);
     console.log(obj.avgAlt);
     let altitude = obj.avgAlt; // distance between object and the ground/sea level
