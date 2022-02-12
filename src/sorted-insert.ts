bf626810b31003bdce38b3cf2c5437d4820b84a5
Nick <nickdloucks@outlook.com>
2022-02-11 19:04:17 -0800

    add types and a few more comments, plus doc

--- src/sorted-insert.ts
+++ src/sorted-insert.ts
@@ -1,21 +1,29 @@
-function getIndexToIns(arr, num) {
-  let number = parseFloat(num); // ensure number is a floating point decimal
+/**
+ * Finds the correct index to insert a number into an array of numbers.
+ * @param arr: Array<number>
+ * @param num: number
+ * @returns index at which to insert : number
+ */
+
+function getIndexToIns(arr: Array<number>, num: number): number {
+  //let number = parseFloat(num); // ensure number is a floating point decimal: this line only needed in plain Javascript, not Typescript
+  let number: number = num;
   let newArr = arr.sort(function (a, b) {
-    // save input array as a sorted array
+    // ensures we are working with a sorted array and there is no mutation outside this function
     return a - b;
   });
-  console.log(newArr);
+
   for (let i = 0; i < newArr.length; i++) {
     if (number <= newArr[i]) {
       // if the number to insert is less than the number at the current index, return 0
-      return 0;
+      return 0; // element is smaller than the rest, so it goes at the beginning
     } else if (number > arr[i] && number <= arr[i + 1]) {
       // otherwise return index at which there is a value greater and a value lower
       // this will be where the number is inserted
       return i + 1;
     }
   }
-  return arr.length;
+  return arr.length; // if this statement is reached, the element belongs at the end
 }
 
 getIndexToIns([40, 60], 50);
