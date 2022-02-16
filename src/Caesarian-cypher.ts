94396d18a7d5290c9d2d1e1ea6c0e84e16403d17
Nick <nickdloucks@outlook.com>
2022-02-15 19:22:31 -0800

    add more types

--- src/Caesarian-cypher.ts
+++ src/Caesarian-cypher.ts
@@ -40,32 +40,32 @@ function rot13(code: string, interval: number): string {
   };
   const ALPH_NUM = {
     //object storing all letters as keys which hold their alphabetical position
-    A: 1,
-    B: 2,
-    C: 3,
-    D: 4,
-    E: 5,
-    F: 6,
-    G: 7,
-    H: 8,
-    I: 9,
-    J: 10,
-    K: 11,
-    L: 12,
-    M: 13,
-    N: 14,
-    O: 15,
-    P: 16,
-    Q: 17,
-    R: 18,
-    S: 19,
-    T: 20,
-    U: 21,
-    V: 22,
-    W: 23,
-    X: 24,
-    Y: 25,
-    Z: 26,
+    'A': 1,
+    'B': 2,
+    'C': 3,
+    'D': 4,
+    'E': 5,
+    'F': 6,
+    'G': 7,
+    'H': 8,
+    'I': 9,
+    'J': 10,
+    'K': 11,
+    'L': 12,
+    'M': 13,
+    'N': 14,
+    'O': 15,
+    'P': 16,
+    'Q': 17,
+    'R': 18,
+    'S': 19,
+    'T': 20,
+    'U': 21,
+    'V': 22,
+    'W': 23,
+    'X': 24,
+    'Y': 25,
+    'Z': 26,
   };
 
   let message = '';
@@ -73,14 +73,10 @@ function rot13(code: string, interval: number): string {
 
   //MAIN ALGORITHM BELOW:
   for (let i = 0; i < codeMsg.length; i++) {
-    let codeChar = codeMsg.charAt(i); //current character from encrypted message
-    if (letter.test(codeChar)) {
-      //test if current code character is a letter
-      const codePos = ALPH_NUM[codeChar]; //position of codeChar in the alphabet
-      let nLetter =
-        codePos <= 13
-          ? NUM_ALPH[codePos + interval]
-          : NUM_ALPH[codePos - interval];
+    let codeChar: string = codeMsg.charAt(i); //current character from encrypted message
+    if (letter.test(codeChar)) { //test if current code character is a letter
+      const codePos: number = ALPH_NUM[codeChar]; //position of codeChar in the alphabet
+      let nLetter = (codePos <= 13) ? NUM_ALPH[codePos + interval] : NUM_ALPH[codePos - interval];
       //if codeChar is in first half of alphabet, add "interval" to the codeChar's position in the alphabet to get the new letter for the de-coded message
       //if codechar is in the secode half of alphabet, subtract interval
       message = message.concat(nLetter);
