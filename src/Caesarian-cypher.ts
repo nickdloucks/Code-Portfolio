13d6fb8be9c65a22218ba582207ded30b65d2bd6
Nick <nickdloucks@outlook.com>
2022-02-16 17:21:57 -0800

    switch to array for letter position, use indexOf()

--- src/Caesarian-cypher.ts
+++ src/Caesarian-cypher.ts
@@ -9,64 +9,15 @@
 function rot13(code: string, interval: number): string {
   // <interval> parameter dynamically changes the offset interval of the cypher.
   let codeMsg = code; //copy input to avoid mutation
-  const NUM_ALPH = {
-    //object storing all letters indexed by alphabetical position to reduce time complexity
-    1: 'A',
-    2: 'B',
-    3: 'C',
-    4: 'D',
-    5: 'E',
-    6: 'F',
-    7: 'G',
-    8: 'H',
-    9: 'I',
-    10: 'J',
-    11: 'K',
-    12: 'L',
-    13: 'M',
-    14: 'N',
-    15: 'O',
-    16: 'P',
-    17: 'Q',
-    18: 'R',
-    19: 'S',
-    20: 'T',
-    21: 'U',
-    22: 'V',
-    23: 'W',
-    24: 'X',
-    25: 'Y',
-    26: 'Z',
+  const NUM_ALPH = {  //object storing all letters indexed by alphabetical position to reduce time complexity
+    1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'E', 6: 'F', 7: 'G', 8: 'H', 9: 'I', 10: 'J', 11: 'K', 12: 'L', 13: 'M',
+    14: 'N', 15: 'O', 16: 'P', 17: 'Q', 18: 'R', 19: 'S', 20: 'T', 21: 'U', 22: 'V', 23: 'W', 24: 'X', 25: 'Y', 26: 'Z',
   };
-  const ALPH_NUM = {
-    //object storing all letters as keys which hold their alphabetical position
-    'A': 1,
-    'B': 2,
-    'C': 3,
-    'D': 4,
-    'E': 5,
-    'F': 6,
-    'G': 7,
-    'H': 8,
-    'I': 9,
-    'J': 10,
-    'K': 11,
-    'L': 12,
-    'M': 13,
-    'N': 14,
-    'O': 15,
-    'P': 16,
-    'Q': 17,
-    'R': 18,
-    'S': 19,
-    'T': 20,
-    'U': 21,
-    'V': 22,
-    'W': 23,
-    'X': 24,
-    'Y': 25,
-    'Z': 26,
-  };
+  // const ALPH_NUM = { //object storing all letters as keys which hold their alphabetical position
+  //   'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13,
+  //   'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26,
+  // };
+  const ALPH_NUM = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
   let message = '';
   let letter = /[A-Z]/i; //regex to determine if a character is a letter
@@ -79,7 +30,7 @@ function rot13(code: string, interval: number): string {
   for (let i = 0; i < codeMsg.length; i++) {
     let codeChar: string = codeMsg.charAt(i); //current character from encrypted message
     if (letter.test(codeChar)) { //test if current code character is a letter
-      const codePos: number = getPosition(ALPH_NUM, codeChar); //position of codeChar in the alphabet
+      const codePos: number = ALPH_NUM.indexOf(codeChar) + 1; //position of codeChar in the alphabet
       let nLetter: string = (codePos <= 13) ? NUM_ALPH[codePos + interval] : NUM_ALPH[codePos - interval];
       //if codeChar is in first half of alphabet, add "interval" to the codeChar's position in the alphabet to get the new letter for the de-coded message
       //if codechar is in the secode half of alphabet, subtract interval
