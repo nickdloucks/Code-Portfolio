08836e11213a39d5b39a09519094eb20fc968068
Nick <nickdloucks@outlook.com>
2022-03-05 12:36:10 -0800

    refactor: ternary operator, comments

--- src/Palindrome-checker.ts
+++ src/Palindrome-checker.ts
@@ -2,30 +2,28 @@
  * Check whether a string is the same backwards as forwards.
  * NOTE: ignores punctuation characters, and letter case.
  * @param str: string. The string to be tested
- * @returns boolean: true if input is a palindrome, false if it's not.
+ * @returns boolean: <true> if input is a palindrome, <false> if it's not.
  */
 
 export default function palindrome(str: string): boolean {
   let testStr = str.toLowerCase(); // copy input to avoid mutation, and convert to lowercase to make comparison easier
-  let noPunc = /[^a-z0-9]/gi; //regex for non-word characters like punctuation or spaces
+  let noPunc = /[^a-z0-9]/gi; //regex for non-word characters like punctuation or spaces (ignore case)
+
+  // I don't think that string.toLowerCase() and the <i> flag on the regex are both necessary, (one or the other should suffice)
+  // but I want to demonsrate in my portfolio that I know about both.
+
   let badChars = noPunc.test(testStr); // test the input for characters to ignore, like punctuation.
   if (badChars) {
     // if there are "bad" characters, replace them with an empty string, effectively deleting them from the <testStr>
     testStr = testStr.replace(noPunc, '');
-    console.log(testStr + ' _punctuation removed');
   }
 
   let backwards: string = testStr.slice(); // copy <testStr> for comparison
   backwards = backwards.split('').reverse().join(''); // reverse the new copy
   // (the method chaining above converts the string to an array of chars, reverses the new array, and concatenates it back to string format)
-  if (backwards == testStr) {
-    // if <backwards> is the same as <testStr>, the input is the same backwards as it is forwards, so it is a palindrome
-    console.log('palindrome');
-    return true;
-  } else {
-    console.log('NOT a palindrome');
-    return false;
-  }
+
+  // if <backwards> is the same as <testStr>, the input is the same backwards as it is forwards, so it is a palindrome
+  return (backwards == testStr) ? true : false;
 }
 
 palindrome('A man, a plan, a canal. Panama'); // test case
