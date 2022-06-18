9b2edee500212382f8f3c4fc539490274f5784ae
Nick <nickdloucks@outlook.com>
2022-06-18 13:03:24 -0700

    used memoization to calc and save prev vals

--- src/Fibonacci.ts
+++ src/Fibonacci.ts
@@ -4,16 +4,33 @@
  * @param nth : number => Get any fibonacci number by it's place in the sequence.
  * @returns 
  */
-function calcNthFib(nth : number): number{
-    let memo: number[] = [];
+function calcNthFib(nth : number, all?: boolean): number{ // provide the target position of a fibonacci number in the sequence
+    // optional <all> param is for requesting the list of fibonacci numbers up to the specified position
+    let memo: number[] = [0, 1, 1];
     let nthFib : number = nth;
 
-    if (nthFib <= 1){
+    if(nthFib <= 1){ // no inputs should be less than 1, and the first fibonacci number is 0
+        return memo[0];
+    }
+    if(nthFib <= 3){ // the second and third fibonnaci number are both 1
         return 1;
     }else{
-        // TODO: may need to compare to length of memo before calculating new value
-        let newResult = calcNthFib(nthFib - 1) + calcNthFib(nthFib - 2);
-        memo.push(newResult);
+        let twoBeforeNth = memo[nthFib - 3]; // it's -3 to account for zero-based indexing of the memo array. 
+        // Example: 2 positions before the third fibonacci is 0
+        if(!twoBeforeNth){ // calculate the fibonacci number two before the desired position if it hasn't be calculated already
+            memo.push(calcNthFib(nthFib - 2));
+            twoBeforeNth = memo[memo.length - 1];
+        }
+
+        let oneBeforeNth = memo[nthFib - 2]; // it's -2 to account for zero-based indexing of the memo array. 
+        // Example: 1 position before the fourth fibonacci is 1 (the second 1 appearing in the memo array at memo[2])
+        if(!oneBeforeNth){ // calculate the previous fibonacci number if it hasn't been calculated already
+            memo.push(calcNthFib(nthFib - 1));
+            oneBeforeNth = memo[memo.length - 1];
+        }
+        // prior to this stage, all previous fibonacci numbers will have been calculated thru recursion and saved in the memo[]
+        let newResult = twoBeforeNth + oneBeforeNth;
+        memo.push(newResult); // save the newly calculated result for future reference and return
         return newResult;
     }
 }
