1e6b7c5fee0f8b2b3362d43fe25b9f888bcdf1db
Nick <nickdloucks@outlook.com>
2022-06-23 13:32:06 -0700

    version for words, not sentences

--- developing/palindrome-recursive.ts
+++ developing/palindrome-recursive.ts
@@ -1,4 +1,7 @@
 // recursive version of palindrome checker
+// this version does not yet account for differences in capitalization nor spaces between words
+// future versions will be useable on sentances, not just single words, and will ignore letter case
+
 export default function palindromeRecursive(str: string): boolean{
     if(!str.length || str.length === 1){
         return true; // if the input is an empty string or a single character, 
@@ -8,8 +11,15 @@ export default function palindromeRecursive(str: string): boolean{
     let nStr = str; // copy input to avoid mutation
 
     function compareEdges(word: string): boolean{
+        // determines whether the first and last character match
         return(word.charAt(0) === word.charAt(word.length - 1)) ? true : false;
     }
 
-    
+    if(compareEdges(nStr)){
+        // if first and last char match, move inward towards the middle of the string and recurse
+        nStr = nStr.slice(1, nStr.length - 1);
+        return palindromeRecursive(nStr);
+    }
+
+    return false;
 }
